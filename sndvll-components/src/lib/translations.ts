56e767bc1beaae1b3c5798f0a92e542c87fe7d0f
Emil Sundvall <emil@sndvll.com>
2022-01-14 17:10:26 +0100

    removed all components from repo and added  sndvll-libs repo instead

--- sndvll-components/src/lib/translations.ts
+++ sndvll-components/src/lib/translations.ts
@@ -1,9 +1,18 @@
-export const TRANSLATIONS = {
+export const TRANSLATIONS_EN = {
   ALERT: {
     WARNING_MESSAGE: 'I understand that this can not be undone!',
     BUTTON: {
       OK: 'OK',
       CLOSE:'Close',
+    }
+  }
+}
+export const TRANSLATIONS_SV = {
+  ALERT: {
+    WARNING_MESSAGE: 'Jag förstår att detta inte går att ångra!',
+    BUTTON: {
+      OK: 'OK',
+      CLOSE:'Stäng',
     }
   }
 }
